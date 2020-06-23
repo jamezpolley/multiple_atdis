e3e24f63e1c9a686d3c50d9a0295428608fc7623
James Polley <jp@jamezpolley.com>
2020-06-23 11:28:24 +1000

    Test new newcastle feed

--- scraper.rb
+++ scraper.rb
@@ -2,44 +2,8 @@
 Bundler.require
 
 AUTHORITIES = {
-  bega_valley: {
-    url: "http://datracker.begavalley.nsw.gov.au/ATDIS/1.0/"
-  },
-  ballina: {
-    url: "http://da.ballina.nsw.gov.au/atdis/1.0"
-  },
-  bathurst: {
-    url: "http://masterview.bathurst.nsw.gov.au/atdis/1.0/"
-  },
-  berrigan: {
-    url: "http://datracking.berriganshire.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/"
-  },
-  dubbo: {
-    url: "https://planning.dubbo.nsw.gov.au/atdis/1.0/"
-  },
-  kiama: {
-    url: "https://da.kiama.nsw.gov.au/atdis/1.0"
-  },
-  upper_hunter: {
-    url: "http://onlineservices.upperhunter.nsw.gov.au/atdis/1.0/"
-  },
-  armidale: {
-    url: "https://epathway.newengland.nsw.gov.au/ePathway/Production/WebServiceGateway/atdis/1.0"
-  },
-  ashfield: {
-    url: "http://mycouncil.solorient.com.au/Horizon/@@horizondap_ashfield@@/atdis/1.0/"
-  },
-  leeton: {
-    url: "http://203.58.97.252/Horizon/@@horizondap@@/atdis/1.0/"
-  },
-  muswellbrook: {
-    url: "https://datracker.muswellbrook.nsw.gov.au/atdis/1.0"
-  },
-  walgett: {
-    url: "http://myhorizon.walgett.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/"
-  },
   newcastle: {
-    url: "https://property.ncc.nsw.gov.au/T1PRPROD/WebAppServices/ATDIS/atdis/1.0"
+    url: "https://ncc-test-web.t1cloud.com/T1PRDefault/WebAppServices/ATDIS/atdis/1.0/"
   }
 }
 
